e5268e88cd2ed61078e3ea426928d2ef43c64b6d
saazanpoudel <sazanpoudel1020@gmail.com>
2026-05-31 08:51:28 +0545

    update vite config for deployment preview settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,   // expose on local network (0.0.0.0) so phone can reach it
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
-      },
-    },
+      }
+    }
   },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true,
+    allowedHosts: 'all'
+  }
 })
